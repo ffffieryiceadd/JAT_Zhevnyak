4c0f4c13cf23c0db5a45343ba8c3a482ff0d878b
Oleg Zhevnyak <ffieryice@gmail.com>
2020-04-05 04:15:08 +0300

    Add 3 part of the hometask1

---
+++ hometask1_Zhevnyak/Task1_3.java
@@ -0,0 +1,12 @@
+package by.itacaemy;
+
+public class Task1_3 {
+    //3. Используя for вывести каждое четное число от 2 до 20 включительно и больше 10.
+    public static void main(String[] args) {
+        for (int y = 2; y < 21; y += 2) {
+            System.out.print(y + " ");
+        }
+        System.out.println();
+        System.out.println();
+    }
+}
