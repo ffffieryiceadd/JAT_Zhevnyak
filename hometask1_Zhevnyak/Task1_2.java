1023804332295af876d639810ec38c2d526358d6
Oleg Zhevnyak <ffieryice@gmail.com>
2020-04-05 04:14:49 +0300

    Add 2 part of the hometask1

---
+++ hometask1_Zhevnyak/Task1_2.java
@@ -0,0 +1,13 @@
+package by.itacaemy;
+
+public class Task1_2 {
+    //2. Используя while вывести все числа от 0 до 25 в одну строку через пробел.
+    public static void main(String[] args) {
+        int x = 0;
+        while (x < 26) {
+            System.out.print(x + " ");
+            x++;
+        }
+    }
+}
+
